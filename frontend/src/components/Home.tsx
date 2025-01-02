9d2cd357fd167ede479248ebdf428ce847ded4b6
Kauê Soares <kaue222120@gmail.com>
2025-01-02 20:15:06 -0300

    refactor: styles

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -9,10 +9,10 @@ export const Home = () => {
   }
 
   return (
-    <div>
+    <div style={{ textAlign: "center", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh", width: "100vw", gap: "1rem" }}>
       <h2>Welcome</h2>
       <button onClick={googleLogin}>Login with Google</button>
-      <button onClick={githubLogin}>Login with GitHub</button>
+      {/* <button onClick={githubLogin}>Login with GitHub</button> */}
     </div>
   )
 }
